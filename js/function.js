4a4a41b4f3ae0b3cc913e9d24ef9e5c3af943838
Камила <105364337+Camihak@users.noreply.github.com>
2024-10-25 12:03:54 +0300

    Нужно больше функций

--- js/function.js
+++ js/function.js
@@ -5,40 +5,40 @@ getChecked('проверяемая строка', 18);
 getChecked('проверяемая строка', 10);
 
 const findPalidrom = (string) => {
-  let normString = string.replaceAll(' ', '').toLowerCase();
+  const normString = string.replaceAll(' ', '').toLowerCase();
   let emptyString = '';
-  for (let i = normString.length-1; i >= 0; i--) {
+  for (let i = normString.length - 1; i >= 0; i--) {
     emptyString += normString[i];
   }
   return emptyString === normString;
-}
+};
 
 findPalidrom('топот');
 findPalidrom('ДовОд');
-findPalidrom('Кекс');  // false
-findPalidrom('Лёша на полке клопа нашёл '); // true
+findPalidrom('Кекс');
+findPalidrom('Лёша на полке клопа нашёл ');
 
 
 const getNumber = (string) => {
-  let normString = string.toString();
+  const normString = string.toString();
   let stringWithNumber = '';
   for (let i = 0; i <= normString.length - 1; i++) {
-    if (normString[i] ==='0' || parseInt(normString[i])) {
+    if (normString[i] === '0' || parseInt(normString[i], 10)) {
       stringWithNumber += normString[i];
     }
   }
 
   return stringWithNumber === '' ? NaN : Number(stringWithNumber);
-}
+};
 
-getNumber('2023 год');            // 2023
-getNumber('ECMAScript 2022');     // 2022
-getNumber('1 кефир, 0.5 батона'); // 105
-getNumber('агент 007');           // 7
-getNumber('а я томат');           // NaN
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
 
-getNumber(2023); // 2023
-getNumber(-1);   // 1
-getNumber(1.5);  // 15
+getNumber(2023);
+getNumber(-1);
+getNumber(1.5);
 
 
